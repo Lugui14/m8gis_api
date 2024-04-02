78ffd54473411fc4d573bd8dc9ebe47b3beadc6f
Gabigks <gabrielgks10@gmail.com>
2024-04-01 22:10:46 -0300

    fix: increase representante_legal character limit

--- entities/socio.py
+++ entities/socio.py
@@ -5,7 +5,7 @@ class Socio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cpf_cnpj = db.Column(db.String(15), nullable=False)
     nome_socio = db.Column(db.String(100), nullable=False)
-    representante_legal = db.Column(db.String(8))
+    representante_legal = db.Column(db.String(12))
     nome_representante_legal = db.Column(db.String(100))
     qualificacao_id = db.Column(db.Integer, db.ForeignKey('qualificacao.id'), nullable=False)
     representante_legal_qualificacao_id = db.Column(db.Integer, db.ForeignKey('qualificacao.id'))
