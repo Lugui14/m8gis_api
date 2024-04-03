d98068547d4d36a04355c2eb2af35886362cea4b
Gabigks <gabrielgks10@gmail.com>
2024-04-03 15:24:08 -0300

    fix: nome_fantasia from estabelecimento nullable

--- entities/estabelecimento.py
+++ entities/estabelecimento.py
@@ -6,7 +6,7 @@ class Estabelecimento(db.Model):
     cnpj_basico = db.Column(db.String(8), nullable=False)
     cnpj_ordem = db.Column(db.String(4), nullable=False)
     identificador_matriz_filial = db.Column(db.Integer, nullable=False)
-    nome_fantasia = db.Column(db.String(100), nullable=False)
+    nome_fantasia = db.Column(db.String(100), nullable=True)
     situacao_cadastral = db.Column(db.Integer, nullable=False)
     data_inicio_atividade = db.Column(db.Date, nullable=False)
     empresa_id = db.Column(db.Integer, db.ForeignKey('empresa.id'), nullable=False)
