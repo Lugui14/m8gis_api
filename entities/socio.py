2f33ed4b0da84a841fbc06c97dbd9cf2a1e1f13d
Gabigks <gabrielgks10@gmail.com>
2024-04-03 21:18:00 -0300

    fix: cpf_cnpj nullable true and increased names char limits

--- entities/socio.py
+++ entities/socio.py
@@ -3,10 +3,10 @@ from extensions import db
 class Socio(db.Model):
     __tablename__ = 'socio'
     id = db.Column(db.Integer, primary_key=True)
-    cpf_cnpj = db.Column(db.String(15), nullable=False)
-    nome_socio = db.Column(db.String(100), nullable=False)
+    cpf_cnpj = db.Column(db.String(15), nullable=True)
+    nome_socio = db.Column(db.String(250), nullable=False)
     representante_legal = db.Column(db.String(12))
-    nome_representante_legal = db.Column(db.String(100))
+    nome_representante_legal = db.Column(db.String(250))
     qualificacao_id = db.Column(db.Integer, db.ForeignKey('qualificacao.id'), nullable=False)
     representante_legal_qualificacao_id = db.Column(db.Integer, db.ForeignKey('qualificacao.id'))
     qualificacao = db.relationship('Qualificacao', foreign_keys=[qualificacao_id], backref='socios')
