4a9d00b153871845306ca7066870f368d2331455
Lugui14 <zanelallopes9977@gmail.com>
2024-05-06 22:26:45 -0300

    refactor: estabelecimentos route

--- repositories/estabelecimento_repository.py
+++ repositories/estabelecimento_repository.py
@@ -5,6 +5,7 @@ from entities.empresa import Empresa
 from entities.cnae import Cnae
 from entities.natureza_juridica import NaturezaJuridica
 from .default_repository import DefaultRepository
+from sqlalchemy import or_
 from sqlalchemy.orm import joinedload
 
 class EstabelecimentoRepository(DefaultRepository):
@@ -12,38 +13,38 @@ class EstabelecimentoRepository(DefaultRepository):
     super().__init__(Estabelecimento)
 
   # def get_all(self, page: int):
-  #   return self.entity.query.options(joinedload(self.entity.cnae)).limit(100).offset((page - 1) * 100).all()  
+  #  return Estabelecimento.query.join(Endereco).add_columns(Endereco.latitude, Endereco.longitude).limit(20).offset(page * 20).all()
 
   def get_filtered(self, filters):
     query = Estabelecimento.query.join(Endereco).join(Municipio).join(Empresa)
-    if 'id' in filters:
+    if 'id' in filters.keys():
       query = query.filter(Estabelecimento.id == filters['id'])
-    if 'cnae' in filters:
-      query = query.join(Cnae).filter(Cnae.id == filters['cnae'])
-    if 'situacao' in filters:
+    if 'cnae' in filters.keys():
+      conditions = [Cnae.id == cnae for cnae in filters['cnae']]
+      query = query.join(Cnae).filter(or_(*conditions))
+    if 'situacao' in filters.keys():
       query = query.filter(Estabelecimento.situacao_cadastral == filters['situacao'])
-    if 'matriz' in filters:
+    if 'matriz' in filters.keys():
       query = query.filter(Estabelecimento.identificador_matriz_filial == filters['matriz'])
-    if 'natju' in filters:
-      query = query.join(NaturezaJuridica).filter(NaturezaJuridica.id == filters['natju'])
-    if 'porte' in filters:
+    if 'natju' in filters.keys():
+      conditions = [NaturezaJuridica.id == natju for natju in filters['natju']]
+      query = query.join(NaturezaJuridica).filter(or_(*conditions))
+    if 'porte' in filters.keys():
       query = query.filter(Empresa.porte == filters['porte'])
-    if 'capital_social_min' in filters:
+    if 'capital_social_min' in filters.keys():
       query = query.filter(Empresa.capital_social >= filters['capital_social_min'])
-    if 'data_abertura' in filters:
+    if 'data_abertura' in filters.keys():
       query = query.filter(Estabelecimento.data_inicio_atividade >= filters['data_abertura'])
-    if 'cnpj' in filters:
+    if 'cnpj' in filters.keys():
       query = query.filter(Empresa.cnpj_basico == filters['cnpj'])
-    if 'razao_social' in filters:
+    if 'razao_social' in filters.keys():
       query = query.filter(Empresa.razao_social == filters['razao_social'])
-    if 'numero' in filters:
+    if 'numero' in filters.keys():
       query = query.filter(Endereco.numero == filters['numero'])
-    if 'logradouro' in filters:
+    if 'logradouro' in filters.keys():
       query = query.filter(Endereco.logradouro == filters['logradouro'].upper())
-    if 'cidade' in filters:
+    if 'cidade' in filters.keys():
       query = query.filter(Municipio.descricao == filters['cidade'].upper()).options(joinedload(Estabelecimento.endereco))
-
-    print(query.count())
 
     return query.all()
   
