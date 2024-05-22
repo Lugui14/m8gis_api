62591e613b6b301d6c816880914357f23d7665db
Gabigks <gabrielgks10@gmail.com>
2024-05-22 14:38:34 -0300

    feat: changes in socio table

--- entities/socio.py
+++ entities/socio.py
@@ -7,6 +7,8 @@ class Socio(db.Model):
     nome_socio = db.Column(db.String(250), nullable=False)
     representante_legal = db.Column(db.String(12))
     nome_representante_legal = db.Column(db.String(250))
+    identificador_socio = db.Column(db.String(1), nullable=True)
+    data_entrada_sociedade = db.Column(db.Date, nullable=True)
     qualificacao_id = db.Column(db.Integer, db.ForeignKey('qualificacao.id'), nullable=False)
     representante_legal_qualificacao_id = db.Column(db.Integer, db.ForeignKey('qualificacao.id'))
     qualificacao = db.relationship('Qualificacao', foreign_keys=[qualificacao_id], backref='socios')
